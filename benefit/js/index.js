465a79b4f7679eb844af063345997f1757f9556c
khwoo <yujx110@gmail.com>
2018-07-06 16:34:20 +0900

    android 시스템 fontSize 문제

---
+++ benefit/js/index.js
@@ -0,0 +1,37 @@
+
+var vm = new Vue({
+    el: '.container',
+    data: {
+        productUrl : 'product_list.html?'
+    },
+
+    created:function(){
+
+        var that = this;
+
+        that.$utils_link( that );
+
+    },
+    mounted: function() {
+
+        var that = this;
+
+        that.$utils_location_params( that );
+
+        that.productUrl = that.$utils_addPageParam( that , that.productUrl );
+
+
+    },
+    methods: {
+
+        productlist:function () {
+
+            var that = this;
+
+            location.href = that.productUrl;
+
+        }
+
+    }
+
+});
